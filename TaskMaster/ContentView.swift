c152633a9a0b973f056ea86dc5a64998c792d661
clylessmith <camdenlylessmith@gmail.com>
2024-04-03 14:02:58 -0600

    Initial Commit

---
+++ TaskMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskMaster
+//
+//  Created by Camden Lyles-Smith on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Camden Lyles-Smith on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
