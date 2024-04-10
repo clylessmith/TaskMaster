476146da98dba1c9579feb03b4a5b0dd991e0804
clylessmith <camdenlylessmith@gmail.com>
2024-04-10 16:19:28 -0600

    working today assignment menu, all assign/courses working

---
+++ TaskMaster/Views/AssignmentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AssignmentDetailView.swift
+//  TaskMaster
+//
+//  Created by Camden Lyles-Smith on 4/9/24.
+//
+
+import SwiftUI
+
+struct AssignmentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssignmentDetailView()
+}
